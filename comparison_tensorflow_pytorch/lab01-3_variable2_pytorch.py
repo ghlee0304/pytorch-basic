8384f70cc9a17a56cb5ebe65f386395e8ea9eecd
Gyeonghoon Lee <azureangma@naver.com>
2018-07-18 15:38:55 +0900

    Update lab01-3_variable2_pytorch.py

--- comparison_tensorflow_pytorch/lab01-3_variable2_pytorch.py
+++ comparison_tensorflow_pytorch/lab01-3_variable2_pytorch.py
@@ -2,25 +2,20 @@ import torch
 from torch.autograd import Variable
 
 x = Variable(torch.ones(2, 2), requires_grad=True)
-target = x.mean()
+y = x.mean()
 
 print("\n====== 초깃값 ======")
 print("x : \n", x.data.numpy())
-print("y : ", target.item())
+print("y : ", y.item())
 
 print("\n>>> 학습 시작")
 for epoch in range(5):
-    target.backward()
+    if not x.grad is None:
+        x.grad.data.zero_()
+    y.backward()
     x.data = x.data - 0.1 * x.grad
-    target = x.mean()
+    y_pred = x.mean()
     print("\n======= Epoch : {} =======".format(epoch + 1))
     print("x : \n", x.data.numpy())
     print("x.grad : \n", x.grad.numpy())
-    print("y : ", target.item())
-
-    if not x.grad is None:
-        x.grad.data.zero_()
-
-
-
-
+    print("y : ", y_pred.item())
