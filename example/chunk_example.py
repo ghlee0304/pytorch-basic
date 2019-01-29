acf353a3a83ebfb9967d4053987d0de8008e09bf
Gyeonghoon Lee <azureangma@naver.com>
2019-01-29 11:12:44 +0900

    Update chunk_example.py

--- example/chunk_example.py
+++ example/chunk_example.py
@@ -1,7 +1,7 @@
 import torch
 
 x = torch.FloatTensor([1,2,3,4,5,6,7,8,9,10])
-print(torch.chunk(x, 3, dim=0))
+print(torch.chunk(x, 3, dim=0)) #10/3 = 3.333인데 올림이 되어 4개 4개 2개로 나뉜다.
 print(torch.chunk(x, 5, dim=0))
 
 '''
